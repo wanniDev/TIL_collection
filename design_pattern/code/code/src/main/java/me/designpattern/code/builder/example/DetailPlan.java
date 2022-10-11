5b4a7ce8415569e8239dc7cab5d896990ce7b055
wanniDev <wannidev0928@gmail.com>
2022-10-12 01:34:55 +0900

    Feat: 빌더패턴 예시코드 작성

---
+++ design_pattern/code/code/src/main/java/me/designpattern/code/builder/example/DetailPlan.java
@@ -0,0 +1,11 @@
+package me.designpattern.code.builder.example;
+
+public class DetailPlan {
+	private int day;
+	private String plan;
+
+	public DetailPlan(int day, String plan) {
+		this.day = day;
+		this.plan = plan;
+	}
+}
---
+++ design_pattern/code/code/src/main/java/me/designpattern/code/builder/example/TourPlanBuilder.java
@@ -0,0 +1,12 @@
+package me.designpattern.code.builder.example;
+
+import java.time.LocalDate;
+
+public interface TourPlanBuilder {
+	TourPlanBuilder title(String title);
+	TourPlanBuilder nightsAndDays(int nights, int days);
+	TourPlanBuilder startDate(LocalDate localDate);
+	TourPlanBuilder whereToStay(String whereToStay);
+	TourPlanBuilder addPlan(int day, String plan);
+	TourPlan getPlan();
+}
