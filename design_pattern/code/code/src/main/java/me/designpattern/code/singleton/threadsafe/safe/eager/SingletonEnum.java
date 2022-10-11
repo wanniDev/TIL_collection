578a8b02707341f68b434cba4252129a6c263872
wanniDev <wannidev0928@gmail.com>
2022-10-11 14:26:10 +0900

    Feat: enum 싱글톤 클래스에 필드 추가

--- design_pattern/code/code/src/main/java/me/designpattern/code/singleton/threadsafe/safe/eager/SingletonEnum.java
+++ design_pattern/code/code/src/main/java/me/designpattern/code/singleton/threadsafe/safe/eager/SingletonEnum.java
@@ -2,4 +2,14 @@ package me.designpattern.code.singleton.threadsafe.safe.eager;
 
 public enum SingletonEnum {
 	INSTANCE;
+
+	private int value;
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
 }
