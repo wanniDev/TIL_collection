c70ba282121b4f281db4a2054a8e58599b1e1501
wanniDev <wannidev0928@gmail.com>
2022-10-13 16:11:35 +0900

    Feat: Prototype 예제 작성

---
+++ design_pattern/code/code/src/main/java/me/designpattern/code/prototype/GithubIssue.java
@@ -0,0 +1,65 @@
+package me.designpattern.code.prototype;
+
+import java.util.Objects;
+
+public class GithubIssue implements Cloneable {
+	private int id;
+	private String title;
+
+	private String url;
+
+	private final GithubRepository repository;
+
+	public GithubIssue(GithubRepository repository) {
+		this.repository = repository;
+	}
+
+	public GithubRepository getRepository() {
+		return repository;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		GithubIssue that = (GithubIssue)o;
+		return id == that.id && Objects.equals(title, that.title) && Objects.equals(url, that.url)
+			&& Objects.equals(repository, that.repository);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, title, url, repository);
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+}
---
+++ design_pattern/code/code/src/test/java/me/designpattern/code/prototype/GithubIssueTest.java
@@ -0,0 +1,21 @@
+package me.designpattern.code.prototype;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GithubIssueTest {
+	@Test
+	@DisplayName("clone을 활용한 프로토타입 객체의 경우, equals를 재정의 했을 때는 shallow copy 기반으로 복제를 한다.")
+	void testCloneable() throws Exception {
+		GithubRepository githubRepository = new GithubRepository();
+		GithubIssue githubIssue1 = new GithubIssue(githubRepository);
+		GithubIssue clone = (GithubIssue)githubIssue1.clone();
+
+		assertThat(githubIssue1).isNotSameAs(clone);
+		assertThat(githubIssue1).isEqualTo(clone);
+		assertThat(githubIssue1.getRepository()).isSameAs(clone.getRepository());
+	}
+}
