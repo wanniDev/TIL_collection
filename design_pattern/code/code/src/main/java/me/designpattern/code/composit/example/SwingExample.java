5a0a7ec92644e95cdd6c9b9c9ada04d065c6d92a
wanniDev <wannidev0928@gmail.com>
2022-10-18 23:41:50 +0900

    Feat: 컴포짓 패턴이 들어간 구체적인 사례 swing

---
+++ design_pattern/code/code/src/main/java/me/designpattern/code/composit/example/SwingExample.java
@@ -0,0 +1,22 @@
+package me.designpattern.code.composit.example;
+
+import javax.swing.*;
+
+public class SwingExample {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+
+		JTextField textField = new JTextField();
+		textField.setBounds(200, 200, 200, 40);
+		frame.add(textField);
+
+		JButton button = new JButton("click");
+		button.setBounds(200, 100, 60, 40);
+		button.addActionListener(e -> textField.setText("Hello swing"));
+
+		frame.add(button);
+		frame.setSize(600, 400);
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+}
