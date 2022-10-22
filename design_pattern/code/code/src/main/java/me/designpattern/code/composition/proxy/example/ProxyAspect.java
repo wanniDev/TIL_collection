42897c9521622e87e767677cb0b7ad9ed855af69
wanniDev <wannidev0928@gmail.com>
2022-10-22 17:19:05 +0900

    Refactor: AOP 의존성 추가

--- design_pattern/code/code/src/main/java/me/designpattern/code/composition/proxy/example/ProxyAspect.java
+++ design_pattern/code/code/src/main/java/me/designpattern/code/composition/proxy/example/ProxyAspect.java
@@ -1,5 +1,8 @@
 package me.designpattern.code.composition.proxy.example;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
