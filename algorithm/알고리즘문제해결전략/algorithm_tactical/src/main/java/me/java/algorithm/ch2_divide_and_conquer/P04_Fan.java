6b5a7c6231e898a629ac70ea09eaaa80caa8c611
wanniDev <wannidev0928@gmail.com>
2023-02-15 11:02:53 +0900

    Feat: 카라츠바 응용 팬미팅 1차 풀이

---
+++ algorithm/알고리즘문제해결전략/algorithm_tactical/src/main/java/me/java/algorithm/ch2_divide_and_conquer/P04_Fan.java
@@ -0,0 +1,31 @@
+package me.java.algorithm.ch2_divide_and_conquer;
+
+import java.util.ArrayList;
+
+public class P04_Fan {
+    public int hugs(String members, String fans) {
+        int M = members.length();
+        int F = fans.length();
+
+        ArrayList<Integer> A = new ArrayList<>();
+        ArrayList<Integer> B = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            int d = members.charAt(i) == 'M' ? 1 : 0;
+            A.add(d);
+        }
+
+
+        for (int i = 0; i < F; i++) {
+            int s = members.charAt(i) == 'M' ? 1 : 0;
+            B.add(M - i - 1, s);
+        }
+
+        ArrayList<Integer> C = karatsuba(A, B);
+        int sum = 0;
+        for (int i = M - 1; i < F; i++) {
+            if (C.get(i) == 0)
+                sum++;
+        }
+        return sum;
+    }
+}
