77d0a0ad4ad0a733239337676ef6d075a2c5d0ca
wanniDev <wannidev0928@gmail.com>
2023-02-26 18:35:51 +0900

    Feat: 재귀 마무리, 정렬된 두 연결리스트 병합 재귀 풀이

---
+++ algorithm/알고리즘문제해결전략/algorithm_tactical/src/main/java/me/java/leetcode/ch01_recurrsion/ch01_02_Complexity/P07_MergedTwoLists.java
@@ -0,0 +1,19 @@
+package me.java.leetcode.ch01_recurrsion.ch01_02_Complexity;
+
+import me.java.leetcode.ListNode;
+
+public class P07_MergedTwoLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        } else if (list1.val > list2.val) {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        } else {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+    }
+}
